4b08164a395106d821d02d7f9732ca9e10e03bac
qab62324-blip <qab62324@gmail.com>
2026-05-20 06:00:39 +0300

    Update default sticker pack and author names

--- system/utils.js
+++ system/utils.js
@@ -17,8 +17,8 @@ if (!fs.existsSync(tmp)) fs.mkdirSync(tmp, { recursive: true });
 
 const createSticker = async (buffer, options = {}) => {
   const sticker = new Sticker(buffer, {
-    pack: options.pack || 'ڤـ ـ VA ـ ـا',
-    author: options.author || 'VA',
+    pack: options.pack || '3QAB,
+    author: options.author || '515',
     type: "full",
     quality: options.mime === "image/jpg" ? 100 : 10
   });
